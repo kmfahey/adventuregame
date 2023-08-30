7316ae29bddb40cfbbfa03e20461a3545566c5cf
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-08-30 14:28:34 -0700

    code reformatting with black

--- advgame/__init__.py
+++ advgame/__init__.py
@@ -63,105 +63,139 @@ from advgame.commands import (
     unlock_command,
 )
 from advgame.data import ini_file_texts
-from advgame.elements import AbilityScores, Armor, Character, Chest, Coin, Container, ContainersState, Corpse, Creature, CreaturesState, Door, DoorsState, Doorway, Equipment, EquippableItem, GameState, IniEntry, State, IronDoor, Item, ItemsMultiState, ItemsState, Key, Oddment, Potion, Room, RoomsState, Shield, Wand, Weapon, WoodenDoor
+from advgame.elements import (
+    AbilityScores,
+    Armor,
+    Character,
+    Chest,
+    Coin,
+    Container,
+    ContainersState,
+    Corpse,
+    Creature,
+    CreaturesState,
+    Door,
+    DoorsState,
+    Doorway,
+    Equipment,
+    EquippableItem,
+    GameState,
+    IniEntry,
+    State,
+    IronDoor,
+    Item,
+    ItemsMultiState,
+    ItemsState,
+    Key,
+    Oddment,
+    Potion,
+    Room,
+    RoomsState,
+    Shield,
+    Wand,
+    Weapon,
+    WoodenDoor,
+)
 from advgame.errors import BadCommandError, InternalError
 from advgame.process import CommandProcessor, Context
-from advgame.statemsgs import AmbiguousDoorSpecifierGSM
-from advgame.statemsgs import AmountToDrinkUnclearGSM
-from advgame.statemsgs import AmountToDropUnclearGSM
-from advgame.statemsgs import AmountToPickUpUnclearGSM
-from advgame.statemsgs import AmountToPutUnclearGSM
-from advgame.statemsgs import AttackHitGSM
-from advgame.statemsgs import AttackMissedGSM
-from advgame.statemsgs import AttackedAndHitGSM
-from advgame.statemsgs import AttackedAndNotHitGSM
-from advgame.statemsgs import BadSyntaxGSM
-from advgame.statemsgs import CantPickUpChestCorpseCreatureOrDoorGSM
-from advgame.statemsgs import CastHealingSpellGSM
-from advgame.statemsgs import CharacterDeathGSM
-from advgame.statemsgs import ClassCantUseItemGSM
-from advgame.statemsgs import ClassRestrictedGSM
-from advgame.statemsgs import ContainerIsClosedGSM
-from advgame.statemsgs import ContainerNotFoundGSM
-from advgame.statemsgs import DisplayHelpForCommandGSM
-from advgame.statemsgs import DisplayInventoryGSM
-from advgame.statemsgs import DoorIsLockedGSM
-from advgame.statemsgs import DisplayRolledStatsGSM
-from advgame.statemsgs import DoorNotPresentGSM
-from advgame.statemsgs import DrankManaPotionGSM
-from advgame.statemsgs import DrankManaPotionWhenNotASpellcasterGSM
-from advgame.statemsgs import DroppedItemGSM
-from advgame.statemsgs import ElementHasBeenClosedGSM
-from advgame.statemsgs import ElementHasBeenLockedGSM
-from advgame.statemsgs import ElementHasBeenOpenedGSM
-from advgame.statemsgs import ElementHasBeenUnlockedGSM
-from advgame.statemsgs import ElementIsAlreadyClosedGSM
-from advgame.statemsgs import ElementIsAlreadyLockedGSM
-from advgame.statemsgs import ElementIsAlreadyOpenGSM
-from advgame.statemsgs import ElementIsAlreadyUnlockedGSM
-from advgame.statemsgs import ElementIsLockedGSM
-from advgame.statemsgs import ElementNotCloseableGSM
-from advgame.statemsgs import ElementNotLockableGSM
-from advgame.statemsgs import ElementNotLockpickableGSM
-from advgame.statemsgs import ElementNotOpenableGSM
-from advgame.statemsgs import ElementNotUnlockableGSM
-from advgame.statemsgs import ElementToCloseNotHereGSM
-from advgame.statemsgs import ElementToLockNotHereGSM
-from advgame.statemsgs import ElementToOpenNotHereGSM
-from advgame.statemsgs import ElementToUnlockNotHereGSM
-from advgame.statemsgs import EnteredRoomGSM
-from advgame.statemsgs import FoeDeathGSM
-from advgame.statemsgs import FoundContainerHereGSM
-from advgame.statemsgs import FoundCreatureHereGSM
-from advgame.statemsgs import FoundDoorOrDoorwayGSM
-from advgame.statemsgs import FoundItemOrItemsHereGSM
-from advgame.statemsgs import FoundNothingGSM
-from advgame.statemsgs import GameBeginsGSM
-from advgame.statemsgs import HaveQuitTheGameGSM
-from advgame.statemsgs import NameOrClassNotSetGSM
-from advgame.statemsgs import ClassSetGSM
-from advgame.statemsgs import InsufficientManaGSM
-from advgame.statemsgs import InvalidClassGSM
-from advgame.statemsgs import InvalidPartGSM
-from advgame.statemsgs import ItemEquippedGSM
-from advgame.statemsgs import ItemNotDrinkableGSM
-from advgame.statemsgs import ItemNotEquippedGSM
-from advgame.statemsgs import DontPossessCorrectKeyGSM
-from advgame.statemsgs import ItemNotFoundGSM
-from advgame.statemsgs import ItemNotFoundInContainerGSM
-from advgame.statemsgs import ItemNotInInventoryGSM
-from advgame.statemsgs import ItemNotInInventoryGSM
-from advgame.statemsgs import ItemOrItemsTakenGSM
-from advgame.statemsgs import ItemPickedUpGSM
-from advgame.statemsgs import ItemUnequippedGSM
-from advgame.statemsgs import LeftRoomGSM
-from advgame.statemsgs import NameOrClassNotSetGSM
-from advgame.statemsgs import CastDamagingSpellGSM
-from advgame.statemsgs import NameSetGSM
-from advgame.statemsgs import StatusOutputGSM
-from advgame.statemsgs import AmountToTakeUnclearGSM
-from advgame.statemsgs import NoCreatureToTargetGSM
-from advgame.statemsgs import NoSuchItemInInventoryGSM
-from advgame.statemsgs import GameStateMessage, DisplayCommandsGSM
-from advgame.statemsgs import NotAllowedNowGSM
-from advgame.statemsgs import NotRecognizedGSM
-from advgame.statemsgs import NotRecognizedGSM
-from advgame.statemsgs import OpponentNotFoundGSM
-from advgame.statemsgs import PutAmountOfItemGSM
-from advgame.statemsgs import TargetHasBeenUnlockedGSM
-from advgame.statemsgs import TargetNotFoundGSM
-from advgame.statemsgs import TargetNotLockedGSM
-from advgame.statemsgs import TriedToDrinkMoreThanPossessedGSM
-from advgame.statemsgs import TryingToDropItemYouDontHaveGSM
-from advgame.statemsgs import TryingToDropMoreThanYouHaveGSM
-from advgame.statemsgs import TryingToPickUpMoreThanIsPresentGSM
-from advgame.statemsgs import TryingToPutMoreThanYouHaveGSM
-from advgame.statemsgs import TryingToTakeMoreThanIsPresentGSM
-from advgame.statemsgs import UnderwentHealingEffectGSM
-from advgame.statemsgs import WonTheGameGSM
-from advgame.statemsgs import DontPossessCorrectKeyGSM
-from advgame.statemsgs import YouHaveNoWeaponOrWandEquippedGSM
-
+from advgame.statemsgs import (
+    AmbiguousDoorSpecifierGSM,
+    AmountToDrinkUnclearGSM,
+    AmountToDropUnclearGSM,
+    AmountToPickUpUnclearGSM,
+    AmountToPutUnclearGSM,
+    AttackHitGSM,
+    AttackMissedGSM,
+    AttackedAndHitGSM,
+    AttackedAndNotHitGSM,
+    BadSyntaxGSM,
+    CantPickUpChestCorpseCreatureOrDoorGSM,
+    CastHealingSpellGSM,
+    CharacterDeathGSM,
+    ClassCantUseItemGSM,
+    ClassRestrictedGSM,
+    ContainerIsClosedGSM,
+    ContainerNotFoundGSM,
+    DisplayHelpForCommandGSM,
+    DisplayInventoryGSM,
+    DoorIsLockedGSM,
+    DisplayRolledStatsGSM,
+    DoorNotPresentGSM,
+    DrankManaPotionGSM,
+    DrankManaPotionWhenNotASpellcasterGSM,
+    DroppedItemGSM,
+    ElementHasBeenClosedGSM,
+    ElementHasBeenLockedGSM,
+    ElementHasBeenOpenedGSM,
+    ElementHasBeenUnlockedGSM,
+    ElementIsAlreadyClosedGSM,
+    ElementIsAlreadyLockedGSM,
+    ElementIsAlreadyOpenGSM,
+    ElementIsAlreadyUnlockedGSM,
+    ElementIsLockedGSM,
+    ElementNotCloseableGSM,
+    ElementNotLockableGSM,
+    ElementNotLockpickableGSM,
+    ElementNotOpenableGSM,
+    ElementNotUnlockableGSM,
+    ElementToCloseNotHereGSM,
+    ElementToLockNotHereGSM,
+    ElementToOpenNotHereGSM,
+    ElementToUnlockNotHereGSM,
+    EnteredRoomGSM,
+    FoeDeathGSM,
+    FoundContainerHereGSM,
+    FoundCreatureHereGSM,
+    FoundDoorOrDoorwayGSM,
+    FoundItemOrItemsHereGSM,
+    FoundNothingGSM,
+    GameBeginsGSM,
+    HaveQuitTheGameGSM,
+    NameOrClassNotSetGSM,
+    ClassSetGSM,
+    InsufficientManaGSM,
+    InvalidClassGSM,
+    InvalidPartGSM,
+    ItemEquippedGSM,
+    ItemNotDrinkableGSM,
+    ItemNotEquippedGSM,
+    DontPossessCorrectKeyGSM,
+    ItemNotFoundGSM,
+    ItemNotFoundInContainerGSM,
+    ItemNotInInventoryGSM,
+    ItemNotInInventoryGSM,
+    ItemOrItemsTakenGSM,
+    ItemPickedUpGSM,
+    ItemUnequippedGSM,
+    LeftRoomGSM,
+    NameOrClassNotSetGSM,
+    CastDamagingSpellGSM,
+    NameSetGSM,
+    StatusOutputGSM,
+    AmountToTakeUnclearGSM,
+    NoCreatureToTargetGSM,
+    NoSuchItemInInventoryGSM,
+    GameStateMessage,
+    DisplayCommandsGSM,
+    NotAllowedNowGSM,
+    NotRecognizedGSM,
+    NotRecognizedGSM,
+    OpponentNotFoundGSM,
+    PutAmountOfItemGSM,
+    TargetHasBeenUnlockedGSM,
+    TargetNotFoundGSM,
+    TargetNotLockedGSM,
+    TriedToDrinkMoreThanPossessedGSM,
+    TryingToDropItemYouDontHaveGSM,
+    TryingToDropMoreThanYouHaveGSM,
+    TryingToPickUpMoreThanIsPresentGSM,
+    TryingToPutMoreThanYouHaveGSM,
+    TryingToTakeMoreThanIsPresentGSM,
+    UnderwentHealingEffectGSM,
+    WonTheGameGSM,
+    DontPossessCorrectKeyGSM,
+    YouHaveNoWeaponOrWandEquippedGSM,
+)
 from advgame.utils import (
     join_strs_w_comma_conj,
     lexical_number_to_digits,
